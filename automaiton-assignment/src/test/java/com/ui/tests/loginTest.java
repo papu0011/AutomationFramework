3b491ae2c45717b352acb3b83d8ce9dc55c5cf88
Ankit <Ankit@Ankit>
2025-12-28 20:12:51 +0530

    checking

---
+++ automaiton-assignment/src/test/java/com/ui/tests/loginTest.java
@@ -0,0 +1,22 @@
+package com.ui.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver wd=new ChromeDriver();
+		wd.get("http://www.automationpractice.pl/index.php");
+		wd.manage().window().maximize();
+		By signInLinkLocator= By.xpath("//a[normalize-space(text())='Sign in']");
+		wd.findElement(signInLinkLocator);
+		wd.close();
+		wd.close();
+
+	}
+
+}
